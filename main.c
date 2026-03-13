0c69fa08d1a8542ddae5948befac5406a076162d
Dusyluc123 <lucasbrsantana@gmail.com>
2026-03-13 09:31:16 -0300

    first commit

---
+++ main.c
@@ -0,0 +1,10 @@
+// Bibliotecas
+#include<stdio.h>
+// Declaração de criação dos nomes das variaveis,  funçoes e inicialisaoes 
+
+// algoritimos/ Programa
+ int main(void){
+    //logiaca do programa 
+    printf("oi");
+    return 0;
+ }
