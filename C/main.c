c8b3b8956d1313a226f2fefebfef6ae9655b9b15
Laboratório FIAP <labsfiap@fiap.com.br>
2026-03-27 09:45:26 -0300

    lucas

--- C/main.c
+++ C/main.c
@@ -5,6 +5,12 @@
 // algoritimos/ Programa
  int main(void){
     //logiaca do programa 
-    printf("oi");
+    int valor_1 = 10;
+    float valor_2= 3;
+    printf("O resultado da soma é:%.0f\n ", valor_1 + valor_2);
+    printf("O resultado da subtração é:%.1f \n", valor_1 - valor_2);
+    printf("O resultado da mutiplicação é:%.2f \n", valor_1 * valor_2);
+    printf("O resultado da divisão  é:%.1f\n", valor_1 / valor_2);
+    printf("O resultado do módulo é:%d\n", valor_1 % (int)valor_2);
     return 0;
  }
